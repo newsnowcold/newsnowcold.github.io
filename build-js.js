634f460c82ecef27b2d12b3ed392f8229bbe398a
Darrel Abello <netzon.darrel@gmail.com>
2016-03-29 16:22:16 +0800

    build-js: Include custom bootstrap.js to be compiled

--- build-js.js
+++ build-js.js
@@ -5,4 +5,5 @@ module.exports = [
     "app/services/mockdata/portfolio.js",
     "app/services/mockdata/skills.js",
     "libs/ngCustomDirective/ngCustomDirective.js",
+    "libs/bootstrap/js/bootstrap.min.js"
 ];
