db7dc73fdb5cc80503eb31beedeb9a6588acc594
Darrel Abello <netzon.darrel@gmail.com>
2017-06-08 17:15:36 +0800

    clean previous files from angular 1 version

--- app/component/bottomSheetNavigation/bottomSheetNavigation.js
+++
@@ -1,8 +0,0 @@
-(function(angular){
-'use strict';
-
-	angular.module('bottomSheetNavigation', [ 
-		'ngMaterial',
-        'ngMdIcons'])
-	
-})(angular)
--- build-js.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-    "app/animation.js",
-    "app/app.js",
-    "app/services/AppDataService.js",
-    "app/services/mockdata/portfolio.js",
-    "app/services/mockdata/skills.js",
-    "libs/ngCustomDirective/ngCustomDirective.js",
-    "libs/bootstrap/js/bootstrap.min.js"
-];
